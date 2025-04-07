765c0069a4f247cd70214916b674107e2d7533d0
jay <jay@jeepies.codes>
2025-04-07 10:52:25 +0100

    removed badges temporarily, added a 'live now' indicator

--- app/components/event-card.tsx
+++ app/components/event-card.tsx
@@ -1,7 +1,9 @@
-import { Calendar, ArrowRight } from 'lucide-react';
-import { Card, CardContent } from './ui/card';
 import { Link } from '@remix-run/react';
-import { convertDateToComfyLocalTime } from '~/lib/utils';
+import { ArrowRight, Calendar } from 'lucide-react';
+import { calculateSecondsUntil, convertDateToComfyLocalTime } from '~/lib/utils';
+import { Card, CardContent } from './ui/card';
+import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@radix-ui/react-tooltip';
+import { Button } from './ui/button';
 
 interface EventCardProps {
   Event: {
@@ -10,21 +12,44 @@ interface EventCardProps {
     Description: string;
     Start: Date;
     End: Date;
+    Tags: string[];
   };
 }
 
 export default function EventCard({ Event }: Readonly<EventCardProps>) {
+  const secondsUntilEventStarts = calculateSecondsUntil(Event.Start);
+
   return (
     <Card>
       <CardContent className="p-0">
-        <div className="w-full h-36 bg-slate-300 rounded-t" />
+        <div className="w-full h-36 bg-slate-300 rounded-t p-2">
+          {secondsUntilEventStarts <= 0 ?
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <div className="w-4 h-4 rounded-full float-right bg-red-600">
+                    <div className="w-2 h-2 m-1 rounded-full float-right bg-red-400" />
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent className="bg-black text-white rounded p-2 opacity-60">
+                  <p>Live now!</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          : <></>}
+        </div>
         <div className="px-2 pb-2">
-          <p className="my-2 text-xl font-bold truncate">{Event.Title}</p>
+          <p className="my-2 text-xl font-bold truncate inline-flex">{Event.Title}</p>
           <p className="text-md my-2 truncate">{Event.Description}</p>
           <span className="text-muted-foreground text-sm inline-flex">
             <Calendar size={20} className="mr-1" />
             {convertDateToComfyLocalTime(Event.Start)} to {convertDateToComfyLocalTime(Event.End)}
           </span>
+          {/* <div className="grid gap-1 grid-cols-3">
+            {Event.Tags.map((tag) => (
+              <Badge className="text-sm">{tag}</Badge>
+            ))}
+          </div> */}
           <Link
             className="float-right text-muted-foreground hover:animate-slide-right hover:text-secondary-foreground"
             to={`/event/${Event.Identifier}`}
