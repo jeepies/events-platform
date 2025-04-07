fc279b1236c00545bb2ce407e2c6c985590a4f5e
jay <jay@jeepies.codes>
2025-04-07 10:21:02 +0100

    added upcoming events to dashboard/index

--- app/routes/_app.dashboard.index.tsx
+++ app/routes/_app.dashboard.index.tsx
@@ -1,6 +1,9 @@
 import { LoaderFunctionArgs, redirect } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
 import { toast } from 'sonner';
+import EventCard from '~/components/event-card';
+import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
+import { getEventsForUserFromID } from '~/services/models/event.server';
 import { getSession, getUserBySession } from '~/services/session.server';
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -12,11 +15,13 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
   if (!user) return redirect('/');
 
-  return { isNewHere, user };
+  const events = await getEventsForUserFromID(user.id);
+
+  return { isNewHere, user, events };
 }
 
 export default function Dashboard() {
-  const { isNewHere, user } = useLoaderData<typeof loader>();
+  const { isNewHere, user, events } = useLoaderData<typeof loader>();
 
   if (isNewHere) toast.success('Your account has been created!');
 
@@ -25,6 +30,26 @@ export default function Dashboard() {
       <h1 className="text-2xl font-bold py-2">
         Welcome, <span className="text-primary">{user.display_name}</span>!
       </h1>
+      <Card className="mt-4">
+        <CardHeader>
+          <CardTitle>Upcoming events</CardTitle>
+        </CardHeader>
+        <CardContent className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 grid-rows-0">
+          {events.length === 0 ?
+            'No events'
+          : events.map((event) => {
+              let _event = {
+                Identifier: event.eventId,
+                Title: event.event.title,
+                Description: event.event.description,
+                Start: event.event.start,
+                End: event.event.end,
+              };
+              return <EventCard Event={_event} />;
+            })
+          }
+        </CardContent>
+      </Card>
     </>
   );
 }
