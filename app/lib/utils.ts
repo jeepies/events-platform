ba9565c88a6dd96d4c406ae206e79a917ca82234
jay <jay@jeepies.codes>
2025-04-07 10:20:43 +0100

    added convertDateToComfyLocalTime

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function convertDateToComfyLocalTime(date: Date) {
+  const localeTime = date.toLocaleTimeString();
+  const localeDate = date.toLocaleDateString();
+  return `${localeDate} @ ${localeTime}`
+} 
