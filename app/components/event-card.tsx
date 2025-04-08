b13cdf889bbad0e7d7734aca97f7737547491750
jay <jay@jeepies.codes>
2025-04-08 06:29:17 +0100

    removed live-now tag

--- app/components/event-card.tsx
+++ app/components/event-card.tsx
@@ -1,6 +1,11 @@
 import { Link } from '@remix-run/react';
 import { ArrowRight, Calendar } from 'lucide-react';
-import { breakSecondsIntoComponents, calculateSecondsUntil, convertDateToComfyLocalTime, turnComponentsIntoString } from '~/lib/utils';
+import {
+  breakSecondsIntoComponents,
+  calculateSecondsUntil,
+  convertDateToComfyLocalTime,
+  turnComponentsIntoString,
+} from '~/lib/utils';
 import { Card, CardContent } from './ui/card';
 import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '@radix-ui/react-tooltip';
 import { Button } from './ui/button';
@@ -25,31 +30,14 @@ export default function EventCard({ Event }: Readonly<EventCardProps>) {
   return (
     <Card>
       <CardContent className="p-0">
-        <div className="w-full h-36 bg-slate-300 rounded-t p-2">
-          {secondsUntilEventStarts <= 0 ?
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger asChild>
-                  <div className="w-4 h-4 rounded-full float-right bg-red-300">
-                    <div className="w-3 h-3 m-0.5 rounded-full float-right bg-red-500" />
-                  </div>
-                </TooltipTrigger>
-                <TooltipContent className="bg-black text-white rounded p-2 opacity-60">
-                  <p>Live now!</p>
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-          : <></>}
-        </div>
+        <div className="w-full h-36 bg-slate-300 rounded-t p-2" />
         <div className="px-2 pb-2">
           <p className="my-2 text-xl font-bold truncate inline-flex">{Event.Title}</p>
           <p className="text-md my-2 truncate">{Event.Description}</p>
           <span className="text-muted-foreground text-sm inline-flex">
             <Calendar size={20} className="mr-1" />
             {secondsUntilEventStarts <= 604800 ?
-              <>
-              {turnComponentsIntoString(secondsAsComponents)}
-              </>
+              <>{turnComponentsIntoString(secondsAsComponents)}</>
             : <>
                 {convertDateToComfyLocalTime(Event.Start)} to {convertDateToComfyLocalTime(Event.End)}
               </>
