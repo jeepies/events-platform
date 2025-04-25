c09690ef3106cc123a1c0b5bcb14a9b7fb6fe5dd
jay <jay@jeepies.codes>
2025-04-25 07:00:43 +0100

    add event creation

--- app/services/models/event.server.ts
+++ app/services/models/event.server.ts
@@ -1,14 +1,17 @@
 import { Event } from '@prisma/client';
 import { prisma } from '../database.server';
 
-export async function createEvent(event: { title: string; description: string, location: string }): Promise<Event> {
+export async function createEvent(
+  event: { title: string; description: string; location: string },
+  time_range: { from: Date; to: Date }
+): Promise<Event> {
   return await prisma.event.create({
     data: {
-        title: event.title,
-        description: event.description,
-        location: event.location,
-        start_time: new Date(),
-        end_time: new Date()
-    }
+      title: event.title,
+      description: event.description,
+      location: event.location,
+      start_time: time_range.from,
+      end_time: time_range.to,
+    },
   });
 }
