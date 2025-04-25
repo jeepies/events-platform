69cc90766e6147319e11c9b27667c03ea64a7f6f
jay <jay@jeepies.codes>
2025-04-25 07:13:10 +0100

    reinstate /events, made /index a snapshot of recently created events

--- app/routes/_app.dashboard.events.tsx
+++ app/routes/_app.dashboard.events.tsx
@@ -1,50 +1,32 @@
-import { LoaderFunctionArgs, redirect } from '@remix-run/node';
-import { Link, useLoaderData } from '@remix-run/react';
+import { Event } from '@prisma/client';
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { Link, redirect, useLoaderData } from '@remix-run/react';
 import { ArrowRight, Calendar, Plus } from 'lucide-react';
-import { toast } from 'sonner';
+import { start } from 'repl';
 import { Button } from '~/components/ui/button';
-import { Card, CardContent } from '~/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
 import { dateObjectToHMString, dateObjectToYMDString } from '~/lib/utils';
 import { prisma } from '~/services/database.server';
 import { getSession, getUserBySession } from '~/services/session.server';
-import { Event } from '@prisma/client';
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  const url = new URL(request.url);
-  const isNewHere = url.searchParams.get('ftx');
-
   const session = await getSession(request.headers.get('Cookie'));
   const user = await getUserBySession(session);
 
-  const events = await prisma.event.findMany({ orderBy: [{ createdAt: 'desc' }], take: 12 });
-
   if (!user) return redirect('/');
 
-  return { isNewHere, user, events };
+  const events = await prisma.event.findMany({ orderBy: [{ createdAt: 'desc' }] });
+
+  return { events, user };
 }
 
-export default function Dashboard() {
-  const { isNewHere, user, events } = useLoaderData<typeof loader>();
-
-  if (isNewHere) toast.success('Your account has been created!');
+export default function Events() {
+  const { events, user } = useLoaderData<typeof loader>();
 
   return (
     <>
-      <h1 className="text-2xl font-bold py-2">
-        Welcome, <span className="text-primary">{user.display_name}</span>!
-      </h1>
-
-      {isNewHere ?
-        <span className="text-muted-foreground text-sm inline-flex">
-          Not fond of being called {user.display_name}? Tell us what to call you in
-          <Link className="text-blue-500 pl-1" to={'/dashboard/settings'}>
-            Settings
-          </Link>
-        </span>
-      : <></>}
-
       <div className="flex justify-between items-center mb-6">
-        <h2 className="text-xl font-semibold">Recent Events</h2>
+        <h2 className="text-xl font-semibold">All Events</h2>
         {user?.is_staff ?
           <Button asChild>
             <Link to="/admin/event/create">
@@ -54,7 +36,7 @@ export default function Dashboard() {
         : <></>}
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 grid-rows-4 gap-6 mb-8">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
         {events.map(({ title, description, start_time, end_time, id }: Event) => {
           const startDate = dateObjectToYMDString(start_time);
           const endDate = dateObjectToYMDString(end_time);
